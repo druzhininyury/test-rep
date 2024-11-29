0330a78bee8dab7517b9cab7a050e90a8ab5803a
Druzhinin Yury <druzhinin.yy@gmail.com>
2024-11-30 01:43:49 +0300

    Some dummy functionality.

---
+++ src/main/java/ru/yandex/practicum/demo/TestDto.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TestDto {
+
+    private String greeting;
+
+}
---
+++ src/test/java/ru/yandex/practicum/demo/TestControllerTest.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = TestController.class)
+public class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void simpleTest() throws Exception {
+        mockMvc.perform(get("/test")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.greeting").exists())
+                .andExpect(jsonPath("$.greeting", is("Hello user!")));
+
+    }
+}
